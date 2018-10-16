87c1f440267eef3663641ae0f23243e98499c424
6heehan <6heehan@gmail.com>
2018-10-16 21:21:07 +0900

    Update 9328.cpp

--- 9328.cpp
+++ 9328.cpp
@@ -14,8 +14,8 @@ int main(){
                 scanf("%d %d",&h,&w);
                 char maze[100][100];
                 int check[100][100];
-                queue<pair<int,int>> q;
-
+//                queue<pair<int,int>> q;
+                stack<pair<int,int>> s;
                 for(int i=0;i<h;i++) {
                         char str[100];
                         scanf("%s",str);
@@ -27,22 +27,81 @@ int main(){
                         }
                 }
 
+                char str[26];
+                sacnf("%s",str);
+                int len = strlen(str);
+                for(int i=0;i<len;i++){
+                        key[i] = str[i];
+                }
+
                 for(int i=0;i<h;i++) {
                         for(int j=0;j<w;j++) {
+                                check[j][i] = 0;
                                 if(maze[j][i]=='*')
                                         check[j][i]=1;
                                 else if(i==0||i==h-1) {
-                                        q.push(pair<j,i>);
+                                        s.push(pair<j,i>);
                                 }
                                 else if(j==0||j==w-1) {
-                                        q.push(pair<j,i>);
+                                        s.push(pair<j,i>);
                                 }
                         }
                 }
+                
+                int res = 0;
+                while(!s.empty()){
 
-                //char key[26];
+                        int x,y;
+                        x = s.top().first;
+                        y = s.top().second;
+                        check[x][y] = 1;
+                        s.pop();
+                        int key_len = strlen(key);
+                        if(x+1 < w && check[x+1][y] == 0) {
+                                if(maze[x+1][y] >= 97 || maze[x+1][y] <= 122) {
+                                        //key found init!!
+                                        key[key_len] = maze[x+1][y];
+                                        while(!s.empty()){
+                                                s.pop();
+                                        }
+                                        
+                                        //init and start point stack push
+                                        
+                                        
+                                } else if(maze[x+1][y] >= 65 || maze[x+1][y] <= 90) {
+                                        //door found
+                                        
+                                        int door = maze[x+1][y];
+                                        for(int a = 0; a < key_len; a++) {
+                                                if(key[a] == door + 32) {
+                                                        
+                                                }
+                                                        
+                                        }
+                                } else if(maze[x+1][y] == '$') {
+                                        //document found
+                                        res++;
+                                        check[x+1][y] == 1;
+                                } else {
+                                        //street? just pass
+                                }
+                                        
+                        }
+                        
+                        if(x-1 > 0 && check[x-1][y] == 0) {
+                        }
+                        
+                        if(y-1 > 0 && check[x][y-1] == 0) {
+                        }
+                        
+                        if(y+1 < h && check[x][y+1] == 0) {
+                        }
+                        
+                        
 
+                }
                 t--;
+
 
 //        printf("%s\n",maze[0]);
 //        printf("%s\n",maze[1]);
