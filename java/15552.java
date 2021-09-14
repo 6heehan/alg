e0ec8e683728076b96e02c1aa013563b43881f41
6heehan <6heehan@gmail.com>
2021-09-14 18:22:04 +0900

    Update 15552.java

--- java/15552.java
+++ java/15552.java
@@ -1,27 +1,29 @@
-package com.tmax.test;
-
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	
+    	StringBuilder sb = new StringBuilder("");
     	try {
-			String test = br.readLine();
-			String test1[] = test.split(" ");
+    		String num = br.readLine();
+    		int n = Integer.valueOf(num);
+    		for(int i=0;i<n;i++) {
+    			String test = br.readLine();
+				String test1[] = test.split(" ");
 			
-			int num1 = Integer.parseInt(test1[0]); 
-			int num2 = Integer.parseInt(test1[1]);
-			System.out.println(num1);
-			System.out.println(num2);
-			String sum = String.valueOf(num1+num2);
-			
-			bw.write(sum);
-			
-			bw.flush();
-			bw.close();
-			//System.out.print(test);
+				int num1 = Integer.parseInt(test1[0]); 
+				int num2 = Integer.parseInt(test1[1]);
+
+				String sum = String.valueOf(num1+num2);
+				sb.append(sum);
+				sb.append("\n");
+				
+				//bw.write(sum);
+				//bw.flush();
+    		}
+    		System.out.print(sb);
+			//bw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
