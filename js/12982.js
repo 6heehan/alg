204c98673b0e18fec26e900cba43e6b21fe5ab78
6heehan <6heehan@gmail.com>
2026-09-21 20:39:54 +0900

    Update 12982.js

--- js/12982.js
+++ js/12982.js
@@ -1,5 +1,10 @@
-for (let i = 0; i < d.length; i++) {
-    budget -= d[i];
-    if (budget < 0) break;
-    answer++;
+function solution(n, budget) {
+    let answer = 0;
+    d.sort((a,b) => a -b );
+    for (let i = 0; i < d.length; i++) {
+        budget -= d[i];
+        if (budget < 0) break;
+        answer++;
+    }
+    return answer
 }
